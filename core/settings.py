4188297a09cf5adb2fd664b7a523ac1ee6694f5e
Gulmira <gulmiraisakbaeva78@gmail.com>
2024-06-17 12:29:17 +0600

    Добавлен домен в ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,7 @@
 from pathlib import Path
+import os
+
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,7 +16,6 @@ SECRET_KEY = 'django-insecure-f#52nqs)4+b&k9mmzdo3-3*9u@o7=p%a3gwh*mqmy(k4a%sork
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['villa-xy0s.onrender.com', 'localhost', '127.0.0.1']
 
 # Application definition
 
