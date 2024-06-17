f9c422e5f87c69a700143bf161f157710ff897d4
Gulmira <gulmiraisakbaeva78@gmail.com>
2024-06-17 12:52:52 +0600

    Добавлен домен в ALLOWED_HOSTS

--- core/settings.py
+++ core/settings.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import os
 
-ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
-
+# ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
+ALLOWED_HOSTS = ['*']
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
