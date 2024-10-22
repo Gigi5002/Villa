3ffd3ee382de061444b619f57b8d9ed70de7eb09
Gulmira <gulmiraisakbaeva78@gmail.com>
2024-10-22 16:33:26 +0600

    еще не закончила хотела увидет изменения

--- core/settings.py
+++ core/settings.py
@@ -1,29 +1,12 @@
 from pathlib import Path
 import os
-ALLOWED_HOSTS = ['*']  # Позволяет принимать запросы с любых хостов (не рекомендуется для продакшена)
-PORT = 8000  # Порт, который будет использоваться Django
-
-# Или для использования переменной окружения, если Render предоставляет такую возможность:
-
-# ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', 'localhost').split(',')
-# settings.py
-
-# ALLOWED_HOSTS = ['villa-tg1m.onrender.com']
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+ALLOWED_HOSTS = ['*']
+PORT = 8000
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-f#52nqs)4+b&k9mmzdo3-3*9u@o7=p%a3gwh*mqmy(k4a%sork'
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -68,19 +51,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'core.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -100,9 +76,6 @@ AUTH_PASSWORD_VALIDATORS = [
 AUTH_USER_MODEL = 'user.MyUser'
 
 
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
@@ -111,19 +84,10 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-# Настройки статических файлов
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# Настройки файлов медиа (если используются)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
